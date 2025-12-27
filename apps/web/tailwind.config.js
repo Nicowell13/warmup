4c84f7d1b72f36371cfffeb1ab700e90040905d9
unknown <royannico@gmail.com>
2025-12-27 15:17:40 +0700

    get1

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -2,10 +2,9 @@
 module.exports = {
   darkMode: ['class'],
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/lib/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './lib/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     container: {
